ae5c94ecb739fca651a2364f6c7c71733f781bb7
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-11 12:24:09 +0600

    search components

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,10 +1,29 @@
 /* eslint-disable prettier/prettier */
 import { Input } from "@nextui-org/input";
 
+import { SearchIcon } from "../../icons";
+
 const Landing = () => {
   return (
-    <div className="h-[calc(100vh-64px)] bg-[url('/found.jpg')] bg-cover bg-center border-4">
-      <Input />
+    <div className="h-[calc(50vh-64px)] bg-[url('/rw.jpg')] dark:bg-[url('/rb.jpg')] bg-cover bg-center">
+      <div className="pt-32 max-w-xl flex-1 mx-auto">
+        <form className="flex-1">
+          <Input
+            aria-label="Search"
+            className="border rounded-lg"
+            classNames={{
+              inputWrapper: "bg-default-100",
+              input: "text-sm",
+            }}
+            placeholder="Search..."
+            size="lg"
+            startContent={
+              <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
+            }
+            type="text"
+          />
+        </form>
+      </div>
     </div>
   );
 };
